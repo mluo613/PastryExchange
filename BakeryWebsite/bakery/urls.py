e71f4182f4c0bb2d0e41cc3b2b3bce03600ea722
Neha Manu <Neha@d-172-25-98-72.dhcp.virginia.edu>
2018-03-12 17:24:49 -0400

    Adding web server

---
+++ BakeryWebsite/bakery/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^bakeryItem/(?P<pk>\d+)$', views.bakeryItemDetailView.as_view(), name='bakeryItem-detail'),
+]
---
+++ BakeryWebsite/bakery/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import generic
+
+# Create your views here.
+def index(request):
+    return render(request, 'bakery/index.html')
+
+class bakeryItemListView(generic.ListView):
+    model = bakeryItem
+
+class bakeryItemDetailView(generic.DetailView):
+    model = bakeryItem
