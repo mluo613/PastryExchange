d243c4acb24d40289ab79fd7a2093ca3e9a0dd9a
Neha Manu <Neha@d-172-25-98-72.dhcp.virginia.edu>
2018-03-12 19:16:36 -0400

    Fixed docker-compose and some other stuff

--- BakeryWebsite/bakery/views.py
+++ BakeryWebsite/bakery/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
-from django.views import generic
+import urllib.request
+import urllib.parse
+import json
 
 # Create your views here.
 def index(request):
     return render(request, 'bakery/index.html')
 
-class bakeryItemListView(generic.ListView):
-    model = bakeryItem
+def bakeryItemRecentList(request):
+    print ("About to perform the GET request...")
 
-class bakeryItemDetailView(generic.DetailView):
-    model = bakeryItem
+    req = urllib.request.Request('http://exp-api/8000/bakeryItem/')
+
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    resp = json.loads(resp_json)
+
+    print(resp)
+
+def bakeryItemDetails(request):
+    print ("About to perform the GET request...")
+
+    req = urllib.request.Request('http://exp-api/8000/bakeryItem/(?P<pk>\d+)')
+
+    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+    resp = json.loads(resp_json)
+
+    print(resp)
+
+#class bakeryItemListView(generic.ListView):
+ #   model = bakeryItem
+
+#class bakeryItemDetailView(generic.DetailView):
+ #   model = bakeryItem
