83e527be3f441779ba6af0c0ac3a7102a3e22959
root <bhc9av@virginia.edu>
2018-03-12 21:20:25 -0400

    again

--- expservice/expservice/urls.py
+++ expservice/expservice/urls.py
@@ -1,4 +1,7 @@
-from django.conf import settings"""expservice URL Configuration
+from django.conf import settings
+
+"""
+expservice URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
