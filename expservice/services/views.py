53d3632c30a01f80859fb44c08c4a1194b77decf
root <bhc9av@virginia.edu>
2018-03-12 21:16:15 -0400

    pk passed instead

--- expservice/services/views.py
+++ expservice/services/views.py
@@ -39,10 +39,9 @@ def userDetails(request, username):
     JSON_object = json.loads(data.decode(encoding))
     return JSON_object
 """
-def itemDetails(request, item_name, username):
-    item_name = str(item_name)
-    name = str(username)
-    url = 'http://models-api/api/v1/users/' + name + '/' + item_name + '/'
+def itemDetails(request, pk):
+    key = str(pk)
+    url = 'http://models-api/api/v1/items/' + key + '/'
     with urllib.request.urlopen(url) as response:
         json = response.read()
     encoding = json.info().get_content_charset('utf-8')
