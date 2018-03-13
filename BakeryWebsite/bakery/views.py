a86e97f4f48d0cf89e0b98ca87b42d5a0e90f52a
Neha Manu <Neha@d-172-25-98-72.dhcp.virginia.edu>
2018-03-12 21:51:56 -0400

    little stuff

--- BakeryWebsite/bakery/views.py
+++ BakeryWebsite/bakery/views.py
@@ -2,31 +2,35 @@ from django.shortcuts import render
 import urllib.request
 import urllib.parse
 import json
+from django.http import HttpResponse
 
 # Create your views here.
 def bakeryItemDetails(request, pk):
-
-    req = urllib.request.Request('http://exp-api:8000/bakeryItem/' + str(pk) + '/')
-
-    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
-    resp = json.loads(resp_json)
-    context = {
-        'bakeryItem': resp,
-    }
-    return render(request, 'bakery/bakeryItem_detail.html', context)
-
+    try:
+        req = urllib.request.Request('http://exp-api:8000/bakeryItem/' + str(pk) + '/')
+        resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+        resp = json.loads(resp_json)
+        context = {
+            'bakeryItem': resp,
+            }
+        return render(request, 'bakery/bakeryItem_detail.html', context)
+    except:
+        return HttpResponse("Page does not exist.")
 
 def index(request):
+    try:
+        req = urllib.request.Request('http://exp-api:8000/latestItems/')
+        resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+        resp = json.loads(resp_json)
+        if resp == "Something went wrong!":
+            
+        context = {
+            'bakeryItem_list': resp,
+            }
+        return render(request, 'bakery/index.html', context)
 
-    req = urllib.request.Request('http://exp-api:8000/bakeryItem/(?P<pk>\d+)')
-
-    resp_json = urllib.request.urlopen(req).read().decode('utf-8')
-    resp = json.loads(resp_json)
-    context = {
-        'bakeryItem_list': resp,
-    }
-    return render(request, 'bakery/index.html', context)
-
+    except:
+        return HttpResponse("Page does not exist.")
 
 #class bakeryItemListView(generic.ListView):
  #   model = bakeryItem
