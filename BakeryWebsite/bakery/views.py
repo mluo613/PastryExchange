86c5ebdcc5ad33ca7fb4e48866faf30844a200d2
Neha Manu <Neha@d-172-25-98-72.dhcp.virginia.edu>
2018-03-12 23:32:41 -0400

    stuff

--- BakeryWebsite/bakery/views.py
+++ BakeryWebsite/bakery/views.py
@@ -4,24 +4,26 @@ import urllib.parse
 import json
 from django.http import HttpResponse
 
+
 # Create your views here.
 def bakeryItemDetails(request, pk):
     try:
-        req = urllib.request.Request('http://exp-api:8000/bakeryItem/' + str(pk) + '/')
-        #resp_json = urllib.request.urlopen(req).read().decode('utf-8')
-        r1 = urllib.request.urlopen(req)
-        return HttpResponse("Page.")
-        r2 = r1.read()
-        return HttpResponse("Page2.")
-        r3 = r2.decode('utf-8')
-        return HttpResponse("Page3.")
+        req = urllib.request.Request('http://exp-api:8000/services/bakeryItem/' + str(pk) + '/')
+        resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+        #r1 = urllib.request.urlopen(req)
+        #return HttpResponse("Page.")
+        #r2 = r1.read()
+        #return HttpResponse("Page2.")
+        #r3 = r2.decode('utf-8')
+        #return HttpResponse("Page3.")
         resp = json.loads(resp_json)
-        return HttpResponse("Page2.")
+        return HttpResponse(resp_json)
+        #d = dict(resp)
         if resp == "Something went wrong!" or resp == "Item does not exist.":
             return HttpResponse("Item does not exist.")
         
         context = {
-            'bakeryItem': resp,
+            'bakeryItem' : resp_json,
             }
         return render(request, 'bakery/bakeryItem_detail.html', context)
     except:
@@ -29,11 +31,11 @@ def bakeryItemDetails(request, pk):
 
 def index(request):
     try:
-        req = urllib.request.Request('http://exp-api:8000/latestItems/')
+        req = urllib.request.Request('http://exp-api:8000/services/latestItems/')
         resp_json = urllib.request.urlopen(req).read().decode('utf-8')
         resp = json.loads(resp_json)
         #if resp == "Something went wrong!" or resp == "Item does not exist.":
-            
+        #return HttpResponse(resp)    
         context = {
             'bakeryItem_list': resp,
             }
--- expservice/services/views.py
+++ expservice/services/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+
 # Create your views here.
 
 import urllib.request
@@ -26,18 +27,25 @@ def latestItems(request):
 # details 
 def itemDetails(request, pk):
     if request.method == 'GET':
-        try:
+#        try:
             key = str(pk)
-            url = 'http://models-api:8000/api/v1/items/' + key + '/'
-
+            url = 'http://models-api:8000/api/v1/items/' + key
+            
             req = urllib.request.Request(url)
-
+            
             resp_json = urllib.request.urlopen(req).read().decode('utf-8')
-            resp = json.loads(resp_json)
-            return resp
+            #return JsonResponse("Something2.", safe=False)
+            #resp = json.loads(resp_json)
+            #with urllib.request.urlopen(url) as response:
+             #   json = response.read()
+            #encoding = json.info().get_content_charset('utf-8') 
+            #JSON_object = json.loads(data.decode(encoding))                                                                                                                                                         
+            #return JSON_object
+            return JsonResponse(json.JSONDecoder().decode(resp_json), safe=False)
+            #return resp_json
 
-        except:
-            return JsonResponse("Something went wrong.", safe=False)
+#        except:
+#            return JsonResponse("Something went wrong.", safe=False)
 
 
 """                                                                                                                                                                   
